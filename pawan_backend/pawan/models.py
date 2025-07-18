e0fe91c5ba89ab2816719630bd0fee101b76dc76
Govind <govindsys1008@gmail.com>
2025-07-18 20:16:51 +0530

    created user models and migrations

--- pawan_backend/pawan/models.py
+++ pawan_backend/pawan/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
+import uuid
 # Create your models here.
+
+class PawanUser(AbstractUser):
+    email_token = models.UUIDField(default=uuid.uuid4)
+    is_verified = models.BooleanField(default = False)
+    def __str__(self):
+        return self.username
